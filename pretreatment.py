3bf0277017babab171c38c8d279864c96afdba9b
Vladie Werber <vladouwerber@icloud.com>
2026-02-22 21:48:49 +0100

    clean data

---
+++ pretreatment.py
@@ -0,0 +1,13 @@
+# Imports
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    mean_squared_error,
+    r2_score
+)
+from sklearn.linear_model import LogisticRegression, LinearRegression
